0ab5e1817b9ff58e31c1e5698060c2bd273edfe7
Andre Sass Chaves <andre.sass@gmail.com>
2026-09-19 09:52:55 +0100

    Fix Portuguese translation for Titia's bedroom name

--- game/js/data/levels.js
+++ game/js/data/levels.js
@@ -74,7 +74,7 @@ const LEVELS = [
   },
   {
     id: 'titia',
-    name: { en: "Rafaela's bedroom", pt: 'O quarto da Rafaela' },
+    name: { en: "Rafaela's bedroom", pt: 'Quarto da Rafaela' },
     subtitle: { en: 'Sweets, sweets everywhere', pt: 'Doce, doce por todo lado' },
     found: 'foundTitia',
     bgColor: '#f7b3d0',
